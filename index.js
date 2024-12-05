a05db7a3ed099ff754fa75b7fe65dee3dcde1e52
Steamgx <your-bmuraya646@gmail.com>
2024-12-05 03:51:38 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W" },
+    { year: "2014", result: "N/A" },
+    { year: "2013", result: "L" },
+];
+
+function superbowlWin(records) {
+    const winningYear = records.find(record => record.result === "W");
+    return winningYear ? winningYear.year : undefined;
+}
+
+console.log(superbowlWin(record)); // Outputs: "2015"
